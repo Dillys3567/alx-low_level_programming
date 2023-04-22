7dd75ae8fa1be612bcb8938b3179a0882c31f137
Dillys Annan <dillysannan@gmail.com>
2023-04-22 09:33:41 -0700

    two combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 	int num;
 	int num2;
 
-	for (num = '0' ; num <= '9' ; num++)
+	for (num = '0' ; num < '9' ; num++)
 	{
-		for (num2 = num + 1 ; num2 <= '9' ; num++)
+		for (num2 = num + 1 ; num2 <= '9' ; num2++)
 		{
 			if (num != num2)
 			{
