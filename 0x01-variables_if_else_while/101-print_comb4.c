12debbfca75250dd36414cb02e7cea11924b2803
Dillys Annan <dillysannan@gmail.com>
2023-04-22 09:44:59 -0700

    combination of 3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - combination of 3 digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	int num2;
+	int num3;
+
+	for (num = '0' ; num < '9' ; num ++)
+	{
+		for (num2 = num + 1 ; num2 <= '9' ; num2 ++)
+		{
+			for (num3 = num2 + 1 ; num3 <= '9' ; num3++)
+			{
+				if ((num2 != num) != num3)
+				{
+					putchar(num);
+					putchar(num2);
+					putchar(num3);
+					if (num == '7' && num2 == '8')
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
