5a394c432dfd48ca491957bfb47150e9b493c4c4
Dillys Annan <dillysannan@gmail.com>
2023-04-22 08:10:12 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main - print base 10 digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10 ;num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
