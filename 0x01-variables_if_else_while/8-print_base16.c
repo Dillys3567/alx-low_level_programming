568ad0c4a2a2068d3a5bdf646ceefed893b5edd9
Dillys Annan <dillysannan@gmail.com>
2023-04-22 08:37:57 -0700

    hexadecimal characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal characters
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a' ; ch <= 'f' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
