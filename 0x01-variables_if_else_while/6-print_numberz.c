0a25c5fdbe295f08993744287673dd17c0ca0a9f
Dillys Annan <dillysannan@gmail.com>
2023-04-22 08:22:32 -0700

    print numbers with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-	char num;
+	char nums[10]="0123456789";
+	int num;
 
-	for (num = '0' ; num <= '9' ; num++)
+	for (num = 0 ; num <= 9 ; num++)
 	{
-		putchar(num);
+		putchar(nums[num]);
 	}
 	putchar('\n');
 	return (0);
