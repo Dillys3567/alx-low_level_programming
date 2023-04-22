341ecf2dd465c5601cbec9921781b62021c7008a
Dillys Annan <dillysannan@gmail.com>
2023-04-22 08:13:07 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- *
  * main - print base 10 digits
  * Return: Always 0
  */
 int main(void)
 {
-	int num;
+	char num;
 
-	for (num = 0; num < 10 ;num++)
+	for (num = 0 ; num < 10 ; num++)
 	{
 		putchar(num);
 	}
