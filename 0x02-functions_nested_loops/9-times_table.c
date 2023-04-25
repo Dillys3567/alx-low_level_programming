2b02816f9b05b0552a5ed2af81f4945c8defeffb
Dillys Annan <dillysannan@gmail.com>
2023-04-25 12:55:57 -0700

    multiplication

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - multiplication
+ */
+void times_table(void)
+{
+	int num;
+	int multi;
+	int prod;
+	int temp;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (multi = 0; multi <= 9; multi++)
+		{
+			prod = num * multi;
+			temp = prod / 10;
+			if (temp != 0){
+				_putchar(temp + '0');
+			}
+			_putchar((prod % 10) + '0');
+			if (multi == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
