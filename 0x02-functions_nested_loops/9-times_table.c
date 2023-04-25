7a2885ab715ea90661c37e41664974ebb4cacf07
Dillys Annan <dillysannan@gmail.com>
2023-04-25 13:00:30 -0700

    multiplication

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,19 +15,23 @@ void times_table(void)
 		{
 			prod = num * multi;
 			temp = prod / 10;
-			if (temp != 0){
+			if (temp != 0)
+			{
 				_putchar(temp + '0');
 			}
 			_putchar((prod % 10) + '0');
 			if (multi == 9)
-			{
 				_putchar('\n');
+			else if(temp == 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
 		}
 	}
 }
