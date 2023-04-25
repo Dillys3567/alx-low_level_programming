3aba83d1319a875efed4c861e6c3be493d98247d
Dillys Annan <dillysannan@gmail.com>
2023-04-25 12:10:51 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * _abs - absolute value
+ * @num: takes an integer
+ * Return: Returns absolute value
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return num;
+	}
+	else
+	{
+		return (-num);
+	}
+}
