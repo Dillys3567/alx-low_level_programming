f3d24ecfdeb3003ae72cf805c03bdfba9e4a60a4
Dillys Annan <dillysannan@gmail.com>
2023-04-25 09:11:29 -0700

    alphanumeric check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphanumeric
+ * @c: charcater
+ * Return: 1 if alphanumeric and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	int ch;
+
+	ch = c;
+	if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
