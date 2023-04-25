0265de7ca60dcd55520e67f38997a47d136d5307
Dillys Annan <dillysannan@gmail.com>
2023-04-25 14:44:53 -0700

    fibonnaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,26 +5,26 @@
  */
 int main(void)
 {
-        int count, b, b2;
-        long int num1, num2, fib, fib2, num11, num22;
+	int count, b, b2;
+	 long int num1, num2, fib, fib2, num11, num22;
 
-        num1 = 1;
+	num1 = 1;
 	num2 = 2;
-        b =  b2 = 1;
-        printf("%ld, %ld", num1, num2);
-        for (count = 0; count < 50; count++)
-        {
-                if (b)
+	b =  b2 = 1;
+	printf("%ld, %ld", num1, num2);
+	for (count = 0; count < 48; count++)
+	{
+		if (b)
 		{
 			fib = num1 + num2;
-                        printf(", %ld", fib);
-                        num1 = num2;
-                        num2 = fib;
-                }
-                else
-                {
-                        if (b2)
-                        {
+			printf(", %ld", fib);
+			num1 = num2;
+			num2 = fib;
+		}
+		else
+		{
+		if (b2)
+			{
                                 num11 = num1 % 1000000000;
                                 num22 = num2 % 1000000000;
                                 num1 = num1 / 1000000000;
