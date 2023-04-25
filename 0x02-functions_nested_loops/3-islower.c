89692d4faa1374a278d2c8520977db0769349a43
Dillys Annan <dillysannan@gmail.com>
2023-04-25 08:59:26 -0700

    check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * Return:  1  if lower and 0 otherwise
+ */
+int _islower(int c)
+{
+	int ch;
+
+	ch = c;
+	if (ch >= 97 || ch <=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
