03e5431ce392aba6a3047b39a57a23f6b3790726
Dillys Annan <dillysannan@gmail.com>
2023-04-25 15:47:41 -0700

    multiplication

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,26 @@ void times_table(void)
 	int num;
 	int multi;
 	int prod;
-	int temp;
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (multi = 0; multi <= 9; multi++)
+		putchar('0');
+		for (multi = 1; multi <= 9; multi++)
 		{
 			prod = num * multi;
-			temp = prod / 10;
-			if (temp != 0)
+			_putchar(',');
+			_putchar(' ');
+			if (prod <= 9)
 			{
-				_putchar(temp + '0');
-			}
-			_putchar((prod % 10) + '0');
-			if (multi == 9)
-				_putchar('\n');
-			else if (temp == 0)
-			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				_putchar(prod + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
