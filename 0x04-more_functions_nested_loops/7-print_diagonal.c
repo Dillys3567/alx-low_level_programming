1834302e22fadd82fc1d70032d557e9baa4be071
Dillys Annan <dillysannan@gmail.com>
2023-04-27 15:57:58 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,20 +8,17 @@ void print_diagonal(int n)
 	int count;
 	int spaces;
 
-	if ((n != 0) || !(n < 0))
+	for (count = 0; count < n; count++)
 	{
-		for (count = 0; count < n; count++)
+		for (spaces = 0; spaces < count; spaces++)
 		{
-			for (spaces = 0; spaces < count; spaces++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
+			_putchar('');
+		}
+		_putchar('\\');
+		if (count < (n -1 ))
+		{
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
