faea1bdc3054e7ffba552c8d724611ee0f9c81a0
Dillys Annan <dillysannan@gmail.com>
2023-04-27 21:39:58 -0700

    print int

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: the integer
+ */
+void print_number(int n)
+{
+	int num, pv, count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = n * -1;
+	}
+	else
+	{
+		num = n;
+	}
+	pv = num;
+	count = 1;
+
+	while(pv > 9)
+	{
+		pv /= 10;
+		count *= 10;
+	}
+
+	for (;count >= 1; count /=10)
+	{
+		_putchar(((num / count) % 10) + '0');
+	}
+}
