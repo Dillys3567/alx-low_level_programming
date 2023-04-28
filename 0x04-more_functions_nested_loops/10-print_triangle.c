53fcec8bfb7f919a39108956e30fda9463df7465
Dillys Annan <dillysannan@gmail.com>
2023-04-27 22:15:58 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: number of lines
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < (size - a); b++)
+		{
+			_putchar(' ');
+		}
+		for (b-- ; b < size ; b++)
+		{
+			_putchar('#');
+		}
+		if (a < (size - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
