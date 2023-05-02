70c6f4a44c981c86f87633213135da7edb305031
Dillys Annan <dillysannan@gmail.com>
2023-05-02 15:10:57 -0700

    half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print 2nd half of string
+ * @str: string variable
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int c = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (c % 2 != 0)
+		c = (len - 1) / 2;
+	else
+		c = len / 2;
+	for (; c < len; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
