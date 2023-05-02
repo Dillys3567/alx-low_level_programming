aa24be749ea344adb0adc5fb9286258d9d9278da
Dillys Annan <dillysannan@gmail.com>
2023-05-02 13:43:52 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - check string length
+ * @s: string to be checked
+ * Return: an int value of the length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s);
+
+	return (len - 1);
+}
