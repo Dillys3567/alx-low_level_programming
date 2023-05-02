5d38d6328976b3791e0e899eefbc4c74c28ef028
Dillys Annan <dillysannan@gmail.com>
2023-05-02 14:22:54 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string pointer
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	while (len != 0)
+	{
+		_putcahr(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
