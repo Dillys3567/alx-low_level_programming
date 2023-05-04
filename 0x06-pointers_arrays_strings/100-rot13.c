bff45c51a94c779ae146a2aeef7f4b79507d9953
Dillys Annan <dillysannan@gmail.com>
2023-05-04 15:00:18 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *rot13 - rot13
+ * @n: array
+ * Return: array
+ */
+char *rot13(char *n)
+{
+	int x, y;
+	char lett = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char lettrot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (n[x] == lett[y])
+			{
+				n[x] = lettrot13[y];
+				break;
+			}
+		}
+	}
+	return (n);
+}
