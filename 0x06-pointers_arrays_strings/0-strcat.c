8c62baae9c67991f18d482724499b62889ec3c6f
Dillys Annan <dillysannan@gmail.com>
2023-05-04 11:31:37 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,28 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len1, len2, i, j;
-
-	len1 = len2 = i =  0;
-
-	while (dest[i] != '\0')
+	int len1, len2;
+	
+	len1 = len2 = 0;
+	while (dest[len1] != '\0')
 	{
 		len1++;
-		i++;
 	}
-	i = 0;
-	while (src[i] != '\0')
+	while (src[len2] != '\0')
 	{
+		dest[len1] = src[len2];
 		len2++;
-		i++;
+		len1++;
 	}
-
-	for (i = 0; i <= len2; i++)
-	{
-		for (j = len1; j <= (len1 + len2); j++)
-		{	
-			dest[] = src[i];
-		}
-	}
-	dest[(len1 - 1 + len2)];
+	dest[len1] = '\0';
+	return (dest);
 }
