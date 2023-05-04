ac1a2615ddca6f8e4023920a44f881eddbe3b7a0
Dillys Annan <dillysannan@gmail.com>
2023-05-04 14:41:51 -0700

    encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *leet - encode string
+ * @n: string
+ * Return: pointer
+ */
+char *leet(char *n)
+{
+	int x, y;
+
+	char arr1[] = "aAeEoOtTlL";
+	char arr2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == arr1[y])
+			{
+				n[x] = arr2[y];
+			}
+		}
+	}
+	return (0);
+}
