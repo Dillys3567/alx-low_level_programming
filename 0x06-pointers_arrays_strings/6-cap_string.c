436e5614dd4d009115ddf3e3137e5b105c583b31
Dillys Annan <dillysannan@gmail.com>
2023-05-04 12:47:00 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *cap_string - capitalize
+ * @n: string
+ * Return: pointer
+ */
+char *cap_string(char *n)
+{
+	int x = 0;
+
+	while (n[x] != '\0')
+	{
+		while (!(n[x] >= 'a' && n[x] <= 'z'))
+			x++;
+		if (n[x - 1] == ' ' ||
+		n[x - 1] == '\t' ||
+		n[x - 1] == '\n' ||
+		n[x - 1] == ',' ||
+		n[x - 1] == ';' ||
+		n[x - 1] == '.' || n[x - 1] == '!' ||
+		n[x - 1] == '?' || n[x - 1] == '"' || n[x - 1] == '(' ||
+		n[x - 1] == ')' || n[x - 1] == '{' || n[x - 1] == '}' || x == 0)
+			n[x] -= 32;
+		x++;
+	}
+}
