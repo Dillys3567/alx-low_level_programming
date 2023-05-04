41e10c8e484e546947eef84350b18cc483b2f5b6
Dillys Annan <dillysannan@gmail.com>
2023-05-04 11:58:41 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - copy a string
+ * @dest: destination
+ * @src: source
+ * @n: number of characters
+ * Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (len < n && src[len] != '/0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	while (len < n)
+	{
+		dest[len] = '\0';
+		len++;
+	}
+	return (dest);
+}
