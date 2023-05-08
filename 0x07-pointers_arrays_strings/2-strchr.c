24d18d8bc375f050e0ef8d229092200ae1e0df7c
Dillys Annan <dillysannan@gmail.com>
2023-05-08 06:14:45 -0700

    check for character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
 	int len = 0;
 
-	while (s[len] != '\0')
+	for (; s[len] >= '\0'; len++)
 	{
-		len++;
-	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
+		if (s[len] == c)
+			return (&s[len]);
 	}
 	return (0);
+}
