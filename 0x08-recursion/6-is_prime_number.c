ff209a8fa697c6e47a3d386ff746f83fcf983d77
Dillys Annan <dillysannan@gmail.com>
2023-05-10 18:34:11 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * primes - check prime
+ * @num: number
+ * @divisor: divisor
+ * Return: result
+ */
+int primes(int num, int divisor)
+{
+	if (num <= 2)
+	{
+		return (num == 2);
+	}
+	if (divisor == 1)
+	{
+		return (1);
+	}
+	if (num % divisor == 0)
+	{
+		return (0);
+	}
+}
+
+/**
+ * is_prime_number - prime number
+ * @n: number
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (primes(n, n / 2));
+}
