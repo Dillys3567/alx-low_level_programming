db054f3c78d26ea3c96f51f336478d7869390695
Dillys Annan <dillysannan@gmail.com>
2023-05-22 16:01:19 -0700

    new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
