72a8a691ea9619b9b3ea642ae7a6c188f66828cd
Dillys Annan <dillysannan@gmail.com>
2023-05-24 16:11:32 -0700

    many fnction pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+#include "3-calc.h"
+/**
+ * op_add - sum 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a - b
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a * b
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a/b
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a mod b
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
