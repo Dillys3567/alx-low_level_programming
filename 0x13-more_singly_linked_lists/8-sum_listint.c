51a2778a5910b358def130762cd36d67077bfcdd
Dillys Annan <dillysannan@gmail.com>
2023-06-05 19:36:17 -0700

    sum of list elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of list
+ * @head: head
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return(sum);
+	while (head->next != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	sum += head->n;
+	return (sum);
+}
