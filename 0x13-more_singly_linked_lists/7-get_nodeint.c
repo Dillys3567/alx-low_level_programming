09693544925efd9ff9a24b79623fb99dfba7e073
Dillys Annan <dillysannan@gmail.com>
2023-06-05 19:20:34 -0700

    element at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get node at index
+ * @head: head
+ * @index: the index
+ * Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *t = head;
+	unsigned int c;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	for (c = 1; c <= index; c++)
+	{
+		t = t->next;
+	}
+	return (t);
+}
