aa3241f3a543cd0a46664a50803bad83d1085da6
Dillys Annan <dillysannan@gmail.com>
2023-06-08 16:18:49 -0700

    endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - function to check endian
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *ch = (char *) & x;
+
+	return (*ch);
+}
