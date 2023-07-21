0d5e52920088e37cb49d88137f544ca9db9dfdef
Dillys Annan <dillysannan@gmail.com>
2023-07-21 09:09:51 -0700

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - print list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *t;
+
+	t = h;
+	while(t)
+	{
+		printf("%d\n", t->n);
+		t = t->n;
+		n++;
+	}
+	return (n);
+}
