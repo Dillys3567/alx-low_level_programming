b34607e8392c84e54833b5855253357cbbf3a3ef
Dillys Annan <dillysannan@gmail.com>
2023-07-24 13:48:48 -0700

    python dynamic

---
+++ 0x18-dynamic_libraries/pyfuncs.c
@@ -0,0 +1,52 @@
+#include "pyfuncs.h"
+/**
+ * add - add 
+ * @a: a
+ * @b: b
+ * Return: result of add
+ */
+int add(a, b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub
+ * @a: a
+ * @b: b
+ * Return: result of sub
+ */
+int sub(a, b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply
+ * @a: a
+ * @b: b
+ * Return: a * b
+ */
+int mul(a, b)
+{
+	int c = a * b;
+	return (c);
+}
+/**
+ * div - divide
+ * @a: a
+ * @b: b
+ * Return: result of div
+ */
+int div(a, b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: a
+ * @b: b
+ * Return: result of mod
+ */
+int mod(a, b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/pyfuncs.h
@@ -0,0 +1,10 @@
+#ifndef PYFUNCS_C
+#define PYFUNCS_C
+
+int add(a, b);
+int sub(a, b);
+int mul(a, b);
+int div(a, b);
+int mod(a, b);
+
+#endif
